78e7e0a9f0a33d4342210da3d86dd2c7dcfbff2a
unknown <aminkhademi8@gmail.com>
2020-04-21 12:51:17 +0430

    bottom navigatiuon changed

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodinfo/src/pages/favorite_page.dart';
 import 'package:foodinfo/src/pages/profile_page.dart';
@@ -37,23 +39,44 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (int index) {
+      bottomNavigationBar: BottomNavyBar(
+        showElevation: true,
+        selectedIndex: currentTabIndex,
+        onItemSelected: (index){
           setState(() {
             currentTabIndex = index;
             currentPage = pages[index];
           });
         },
-        currentIndex: currentTabIndex,
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart), title: Text('Orders')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), title: Text('Favorite')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text('Profile')),
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            title: Text('Home'),
+            inactiveColor: Colors.black45,
+            icon: Icon(Icons.home),
+              textAlign: TextAlign.center,
+            activeColor: Colors.orangeAccent
+          ),
+          BottomNavyBarItem(
+              title: Text('Orders'),
+              inactiveColor: Colors.black45,
+              textAlign: TextAlign.center,
+              icon: Icon(Icons.shopping_cart),
+              activeColor: Colors.red
+          ),
+          BottomNavyBarItem(
+              title: Text('Favorite'),
+              inactiveColor: Colors.black45,
+              textAlign: TextAlign.center,
+              icon: Icon(Icons.favorite),
+              activeColor: Colors.cyan
+          ),
+          BottomNavyBarItem(
+              title: Text('Profile'),
+              icon: Icon(Icons.person),
+              inactiveColor: Colors.black45,
+              textAlign: TextAlign.center,
+              activeColor: Colors.green
+          ),
         ],
       ),
       body: currentPage,
